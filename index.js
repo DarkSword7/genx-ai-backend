a1fbf4ff1b3e31b48d5f4dab8efb156943fcbb87
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-10-27 15:02:16 +0530

    completed

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import express from "express";
+import cors from "cors";
 import ImageKit from "imagekit";
+import mongoose from "mongoose";
+import UserChats from "./models/userChats.js";
+import Chat from "./models/chats.js";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -15,6 +38,109 @@ app.get("/api/upload", (req, res) => {
   res.send(result);
 });
 
+app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+  const { text } = req.body;
+
+  try {
+    // Create a new chat
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    });
+
+    const savedChat = await newChat.save();
+
+    // Check if the user has a chat
+    const userChats = await UserChats.find({ userId: userId });
+
+    //if doesn't have a chat, create a new userChats
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [{ _id: savedChat._id, title: text.substring(0, 40) }],
+      });
+
+      await newUserChats.save();
+    } else {
+      //if has a chat, update the userChats
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: { _id: savedChat._id, title: text.substring(0, 40) },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error creating chat!");
+  }
+});
+
+app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const userChats = await UserChats.find({ userId });
+    res.status(200).send(userChats[0].chats);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error fetching user chats!");
+  }
+});
+
+app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const chat = await Chat.findOne({ _id: req.params.id, userId });
+    res.status(200).send(chat);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error fetching chat!");
+  }
+});
+
+app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  const { question, answer, img } = req.body;
+
+  const newItems = [
+    ...(question
+      ? [{ role: "user", parts: [{ text: question }], ...(img && { img }) }]
+      : []),
+    { role: "model", parts: [{ text: answer }] },
+  ];
+
+  try {
+    const updatedChat = await Chat.updateOne(
+      { _id: req.params.id, userId },
+      {
+        $push: {
+          history: {
+            $each: newItems,
+          },
+        },
+      }
+    );
+    res.status(200).send(updatedChat);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error adding conversation!");
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send("Unauthenticated!");
+});
+
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
